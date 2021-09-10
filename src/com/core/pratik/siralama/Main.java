cb16ebb116f326b77d4daf4361449653255241d1
Yasin Beyazlı <yasinbeyazli29@gmail.com>
2021-09-10 06:02:59 +0300

    sayi siralama

---
+++ src/com/core/pratik/siralama/Main.java
@@ -0,0 +1,41 @@
+package com.core.pratik.siralama;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int n1, n2, n3;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Birinci sayıyı giriniz: ");
+        n1 = input.nextInt();
+
+        System.out.print("İkinci sayıyı giriniz: ");
+        n2 = input.nextInt();
+
+        System.out.print("Üçüncü sayıyı giriniz: ");
+        n3 = input.nextInt();
+
+        if (n1 < n2 && n1 < n3) {
+            if (n2 < n3) {
+                System.out.println(n1 + " < " + n2 + " < " + n3);
+            } else {
+                System.out.println(n1 + " < " + n3 + " < " + n2);
+            }
+        } else if (n2 < n1 && n2 < n3) {
+            if (n1 < n3) {
+                System.out.println(n2 + " < " + n1 + " < " + n3);
+            } else {
+                System.out.println(n2 + " < " + n3 + " < " + n1);
+            }
+        } else if (n3 < n1 && n3 < n2) {
+            if (n1 < n2){
+                System.out.println(n3 + " < " + n1 + " < " + n2);
+            } else {
+                System.out.println(n3 + " < " + n2 + " < " + n1);
+            }
+        }
+    }
+}
