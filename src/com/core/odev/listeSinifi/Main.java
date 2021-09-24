9f9c7dbddf46fb45f0d7bf0eb7c977e9742bad21
Yasin Beyazlı <yasinbeyazli29@gmail.com>
2021-09-24 14:07:20 +0300

    liste sinifi 2

--- src/com/core/odev/listeSinifi/Main.java
+++ src/com/core/odev/listeSinifi/Main.java
@@ -7,21 +7,30 @@ public class Main {
         MyList<Integer> liste = new MyList<>();
         System.out.println("Dizideki Eleman Sayısı : " + liste.size());
         System.out.println("Dizinin Kapasitesi : " + liste.getCapacity());
+
         liste.add(10);
         liste.add(20);
         liste.add(30);
         liste.add(40);
-        System.out.println("Dizideki Eleman Sayısı : " + liste.size());
-        System.out.println("Dizinin Kapasitesi : " + liste.getCapacity());
         liste.add(50);
         liste.add(60);
         liste.add(70);
         liste.add(80);
         liste.add(90);
         liste.add(100);
+        liste.add(80);
         liste.add(110);
-        System.out.println("Dizideki Eleman Sayısı : " + liste.size());
-        System.out.println("Dizinin Kapasitesi : " + liste.getCapacity());
+
+        System.out.println("80 değerinin listedeki ilk değeri: " + liste.indexOf(80));
+        System.out.println("80 değerinin listedeki son index'i: " + liste.lastIndexOf(80));
+        System.out.println("Liste boş mu* " + liste.isEmpty());
+
+        MyList<Integer> liste2 = liste.sublist(2, 5);
+        System.out.println("Yeni listenin ilk elamanı: " + liste2.get(0));
+
+        System.out.println("2 değeri listede var mı? " + liste.contains(2));
+        System.out.println("####### Elemanların Listesi ########");
+        System.out.println(liste.toString());
 
     }
 }
--- src/com/core/odev/listeSinifi/MyList.java
+++ src/com/core/odev/listeSinifi/MyList.java
@@ -1,5 +1,7 @@
 package com.core.odev.listeSinifi;
 
+import java.util.Arrays;
+
 public class MyList<T> {
     private int capacity = 10;
     private int size = 0;
@@ -21,6 +23,7 @@ public class MyList<T> {
                 newArr[i] = this.arr[i];
             }
             newArr[this.arr.length] = value;
+            this.arr = newArr;
             size++;
         } else {
             for (int i = 0; i < this.arr.length; i++){
@@ -41,11 +44,117 @@ public class MyList<T> {
         return this.size;
     }
 
-    public void listTheElements(){
-        for (T value: this.arr
-             ) {
-            System.out.println(value);
-        }
+    public T get(int index){
+        T item = this.arr[index];
+        if (item == null) return null;
+        return item;
     }
 
+    public T remove(int index){
+        if (index < 0 || index >= this.arr.length) return null;
+
+        T valueToBeDeleted = this.arr[index];
+
+        for (int i = index; i < this.arr.length - 1; i ++)
+        {
+            this.arr[index] = this.arr[index + 1];
+        }
+
+        this.arr[this.arr.length - 1] = null;
+        this.size -= 1;
+        return valueToBeDeleted;
+
+    }
+
+    public T set(int index, T data)
+    {
+        if (index >= this.arr.length || index < 0)
+        {
+            System.out.println("Invalid index");
+            return null;
+        }
+
+        T returnValue = this.arr[index];
+
+        this.arr[index] = data;
+
+        return returnValue;
+
+    }
+
+    /* Parametrede verilen nesnenin listedeki indeksini verir. Nesne listede yoksa -1 değerini verir. */
+    public int indexOf(T data) {
+        int index = -1;
+        for (int i = 0; i < this.arr.length; i++){
+            if (this.arr[i] == data) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    /* Belirtilen öğenin listedeki son indeksini söyler. Nesne listede yoksa -1 değerini verir. */
+    public int lastIndexOf(T data){
+        int index = -1;
+        for (int i = 0; i < this.arr.length; i++){
+            if (this.arr[i] == data) {
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    /* Listenin boş olup olmadığını söyler. */
+    public boolean isEmpty() {
+        if (this.arr.length == 0 || this.arr[0] == null) return true;
+        return false;
+    }
+
+    /* Parametrede verilen indeks aralığına ait bir liste döner. */
+    public MyList<T> sublist(int start, int finish) {
+
+        if (start > finish || finish > this.arr.length) return null;
+
+        MyList<T> sublist = new MyList<>();
+
+        for (int i = start; i < finish; i++){
+            sublist.add(this.arr[i]);
+        }
+        return sublist;
+    }
+
+    /* Parametrede verilen değerin dizide olup olmadığını söyler. */
+    public boolean contains(T data) {
+        for (T value: this.arr
+             ) {
+            if (value == data) return true;
+        }
+        return false;
+    }
+    /* Listedeki bütün öğeleri siler, boş liste haline getirir. */
+    public void clear() {
+        this.arr = (T[]) new Object[this.capacity];
+    }
+
+    /* Sınıfa ait dizideki elemanları listeleyen bir metot. */
+    @Override
+    public String toString()
+    {
+        String list = "";
+
+        list += "[";
+        for (int i = 0; i < this.arr.length;i++)
+        {
+            if (this.arr[i] != null)
+            {
+                list += this.arr[i];
+
+            }
+            if (i != this.arr.length - 1 && this.arr[i+1] != null) list+=", ";
+        }
+        list += "]";
+
+        return list;
+    }
 }
